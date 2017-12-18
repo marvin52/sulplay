bdd603a02eb5775ed595c3eba1fb3615a00a801c
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-12-18 21:30:52 -0200

    feat: mock artists json and include handlebars cdn

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function hideSidebar(){
   sidebarBg.classList.add("off")
 }
 
-function showView(viewId){
+function showView(viewId, callback){
   viewId = viewId || '';
   [
   'home',
@@ -26,5 +26,27 @@ function showView(viewId){
     } else {
       el.classList.remove('hide')
     }
-  })
+  });
+
+  if(callback) callback();
 }
+
+
+var source   = document.getElementById("lista-bandas-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var banda_ = bandas
+  .sort(function(a, b){
+      if(a.description < b.description) return -1;
+      if(a.description > b.description) return 1;
+      return 0;
+  })
+  .map(function(el){
+    var temp = el
+    temp.image = el.track[0].byArtist.image
+    return temp
+  });
+
+var context = { banda: banda_ };
+var html    = template(context);
+document.querySelector('#bandas').innerHTML = html
